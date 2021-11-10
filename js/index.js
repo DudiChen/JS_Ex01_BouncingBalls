5d10553d6752dd119f25116c301ea9b43f100219
lefonde <alexlef3@gmhail.com>
2021-11-10 15:42:49 +0200

    random ball location

--- js/index.js
+++ js/index.js
@@ -3,35 +3,33 @@ let id = null;
 function myMove() {
     const my_ball = document.getElementById("my_ball");
     const board = document.getElementById("main_board");
-    let pos_x = 0;
-    let pos_y = 0;
-    let dir_x = 1;
-    let dir_y = 1;
+    let dir_x = Math.random() > 0.5 ? 1 : -1;
+    let dir_y = Math.random() > 0.5 ? 1 : -1;
     
     clearInterval(id);
     id = setInterval(frame, 10);
-    
+    randomizeStartLocation(my_ball, board);
     function frame() {
-
-        // if (my_ball.offsetTop + my_ball.offsetHeight == board.offsetHeight - 32 || my_ball.offsetLeft + my_ball.offsetWidth == board.offsetWidth - 32) {
-        //     clearInterval(id);
-        // } else {
-        //     pos_y++;
-        //     pos_x++;
-        //     my_ball.style.top = pos_y + 'px';
-        //     my_ball.style.left = pos_x + 'px';
-        // }
-
-        // if (my_ball.offsetTop + my_ball.offsetHeight == board.offsetHeight - 32 || my_ball.offsetLeft + my_ball.offsetWidth == board.offsetWidth - 32) {
-        //     clearInterval(id);
         
         my_ball.style.top = my_ball.offsetTop + dir_y + 'px';
         my_ball.style.left = my_ball.offsetLeft + dir_x + 'px';
         
-        if(my_ball.offsetTop + my_ball.offsetHeight == board.offsetHeight - 32 || my_ball.offsetTop == 0)
+        if(my_ball.offsetTop + my_ball.offsetHeight == board.offsetHeight || my_ball.offsetTop == 0)
             dir_y = -1 * dir_y
 
-        if(my_ball.offsetLeft + my_ball.offsetWidth == board.offsetWidth - 32 || my_ball.offsetLeft == 0)
+        if(my_ball.offsetLeft + my_ball.offsetWidth == board.offsetWidth || my_ball.offsetLeft == 0)
             dir_x = -1 * dir_x
     }
 }
+
+function randomizeStartLocation(ball, board) {
+    if(Math.floor(Math.random() * 2)) {
+        ball.style.top = Math.floor(Math.random() * (board.offsetHeight - ball.offsetHeight - 1));
+        ball.style.left = Math.random > 0.5 ? 1 : (board.offsetWidth - ball.offsetWidth - 1);
+    }
+    else {
+        ball.style.left = Math.floor(Math.random() * (board.offsetWidth - ball.offsetWidth - 1));
+        ball.style.top = Math.random > 0.5 ? 1 : (board.offsetHeight - ball.offsetHeight - 1);
+    }
+}
+
